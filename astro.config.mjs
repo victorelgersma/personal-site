37805e5ba68eff7ea1fa721885c826c0ceb72fc1
Victor Elgersma <victor.elgersma41@thg.com>
2025-02-19 21:14:38 +0000

    removed tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()]
-    },
     server: {
         port: 3000 // Change this to your desired port number
     }
