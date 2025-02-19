2787138377078b730b3bda42ac5a76c4a97dcfe8
Victor Elgersma <victor.elgersma41@thg.com>
2025-02-19 21:02:45 +0000

    changed default port to 3k

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    server: {
+        port: 3000 // Change this to your desired port number
+    }
+});
